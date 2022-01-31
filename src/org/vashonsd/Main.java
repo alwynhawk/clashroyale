f0d3cb72c535106fa3e98753a6b0f12f47ee1a4d
AlwynStamat <65757280+alwynhawk@users.noreply.github.com>
2022-01-30 19:05:14 -0800

    updated main
    yes

--- src/org/vashonsd/Main.java
+++ src/org/vashonsd/Main.java
@@ -1,48 +1,136 @@
-package org.vashonsd;
-import java.util.Objects;
-import java.util.Scanner;
+package com.company;
 
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Card dragon = new Card("Red Dragon");
-        dragon.updateTrait(Trait.ATHLETIC, false);
-        dragon.updateTrait(Trait.MORNING, false);
-        Card wizard = new Card("Wizard");
-        wizard.updateTrait(Trait.ATHLETIC, false);
-        wizard.updateTrait(Trait.MORNING, true);
-        CardList masterList = new CardList();
-        masterList.addCard(dragon);
-        masterList.addCard(wizard);
-//        System.out.println(masterList);
-        System.out.println(masterList.getCardsWhere(Trait.ATHLETIC, false));
-        System.out.println(masterList.getCardsWhere(Trait.MORNING, true));
-
-        //Wish list for this program:
-        //Learn to store and retrieve the data in a JSON file.
-
-        //Next:
-        //Have two cards duel.
+    public static void main(String[] args) throws IOException, InterruptedException {
 
 
 
-//        System.out.println("take the test to see which clash royale card you are!");
-//        System.out.println("do you want to start? (hint, type yes)");
-//        Scanner scan = new Scanner(System.in);
-//        String object = scan.nextLine();
-//        System.out.println("Question 1: Are you a morning person");
-//        String Q1 = scan.nextLine();
-//
-//        if(Objects.equals(Q1, "yes")) {
-//            System.out.println("yes");
-//        }
-//        else {
-//            System.out.println("no");
-//        }
 
+        System.out.println("How is your day going?: enter good or bad");
+        Scanner scan = new Scanner(System.in);
+        String day = scan.nextLine();
+        if (day.equals("bad")) {
+            System.out.println("Oh no! lets try a breathing exercise! (box breathing)");
+           Thread.sleep(2000);
+            int i = 0;
+            while(i <= 2) {
+                System.out.println("breath in");
+                Thread.sleep(5000);
+                System.out.println("hold");
+                Thread.sleep(5000);
+                System.out.println("breath out");
+                Thread.sleep(5000);
+                System.out.println("hold");
+                Thread.sleep(5000);
+                i++;
+
+            }
+            System.out.println("do you feel better?: enter yes or no");
+            String better = scan.nextLine();
+            if (better.equals("yes")) {
+                System.out.println("Good bye! have a great knight!");
+                System.out.println("  ,   A           {}\n" +
+                        " / \\, | ,        .--.\n" +
+                        "|    =|= >      /.--.\\\n" +
+                        " \\ /` | `       |====|\n" +
+                        "  `   |         |`::`|  \n" +
+                        "      |     .-;`\\..../`;_.-^-._\n" +
+                        "     /\\\\/  /  |...::..|`   :   `|\n" +
+                        "     |:'\\ |   /'''::''|   .:.   |\n" +
+                        "      \\ /\\;-,/\\   ::  |..:::::..|\n" +
+                        "      |\\ <` >  >._::_.| ':::::' |\n" +
+                        "      | `\"\"`  /   ^^  |   ':'   |\n" +
+                        "      |       |       \\    :    /\n" +
+                        "      |       |        \\   :   / \n" +
+                        "      |       |___/\\___|`-.:.-`\n" +
+                        "      |        \\_ || _/    `\n" +
+                        "      |        <_ >< _>\n" +
+                        "      |        |  ||  |\n" +
+                        "      |        |  ||  |\n" +
+                        "      |       _\\.:||:./_\n" +
+                        "      |      /____/\\____\\");
+            }
+            else {
+                System.out.println("have a grape day!!");
+                System.out.println("                                                                         \n" +
+                        "                                    ████                        ▓▓██            \n" +
+                        "                                    ██░░██                  ████░░▓▓            \n" +
+                        "                          ████████████░░░░████          ████░░░░▒▒██            \n" +
+                        "                      ████▒▒▒▒▒▒▒▒▒▒██▒▒░░░░░░██████████░░░░░░▒▒▓▓              \n" +
+                        "                    ▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓██▒▒▒▒░░░░░░░░░░░░░░▒▒▒▒▓▓                \n" +
+                        "                    ██▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒░░░░░░▒▒░░░░▒▒▒▒██                \n" +
+                        "                  ▓▓▓▓▒▒    ▒▒▒▒▒▒▒▒▒▒▓▓▓▓████░░░░▒▒▒▒░░░░░░▒▒▒▒▓▓              \n" +
+                        "                  ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒██░░▒▒▒▒▒▒▒▒░░░░░░░░░░▓▓▓▓▓▓▓▓      \n" +
+                        "                  ██▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒██░░▒▒████▒▒▒▒▒▒░░░░░░░░░░░░▓▓      \n" +
+                        "                  ██▓▓▒▒▒▒▒▒▒▒▒▒▓▓▒▒    ▒▒▒▒██▒▒██▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▓▓        \n" +
+                        "                  ██▓▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒    ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▓▓████████▓▓▓▓          \n" +
+                        "                  ██▓▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒▓▓▓▓▓▓████              \n" +
+                        "                    ██▓▓▓▓▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓██            \n" +
+                        "                    ██▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▓▓▓▓▓▓▒▒▒▒██            \n" +
+                        "                  ▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒▓▓██          \n" +
+                        "                  ██▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒▒▒██          \n" +
+                        "                  ▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▒▒▒▒██          \n" +
+                        "                  ██▓▓▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒▒▒██          \n" +
+                        "                  ▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒██          \n" +
+                        "              ████▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓            \n" +
+                        "            ▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒██            \n" +
+                        "            ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓              \n" +
+                        "          ▓▓▒▒▒▒░░  ▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                  \n" +
+                        "          ▓▓▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓██                        \n" +
+                        "          ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒██                          \n" +
+                        "          ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒██                          \n" +
+                        "          ▓▓▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒██                            \n" +
+                        "          ██▒▒▓▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒██                              \n" +
+                        "            ██▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒████                                \n" +
+                        "            ▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓████████                                    \n" +
+                        "              ▓▓▓▓▓▓▒▒▓▓▒▒▒▒▒▒▓▓▓▓░░░░▒▒░░                                      \n" +
+                        "                  ▓▓▓▓▓▓▓▓▓▓▓▓                                                  \n" +
+                        "                                                                                \n" +
+                        "                                                                                \n" +
+                        "                                                                               ");
+            }
+
+
+
+
+
+        }
+        else {
+            System.out.println("nice, we are going to play a number speed game now");
+            Thread.sleep(2000);
+            System.out.println("Im going to spit out a number, type it in as fast as possible!");
+            Thread.sleep(3000);
+            System.out.println("3");
+            Thread.sleep(1000);
+            System.out.println("2");
+            Thread.sleep(1000);
+            System.out.println("1");
+            System.out.println(returnNumbers());
+            int enteredNum = scan.nextInt();
+            scan.nextLine();
+            if (enteredNum == returnNumbers()) {
+                System.out.println("nice job! see you later!");
+            }
+            else {
+                System.out.println("nice job! see you later!");
+            }
+
+
+
+        }
 
 
     }
+
+    private static int returnNumbers() {
+        Random rand = new Random();
+        int n = rand.nextInt(999);
+        return n;
+    }
+
 }
